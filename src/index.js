126bb55ffebdf18b9ad68d777229362140c36dc9
Alexander Pepper <alexander.pepper@infopark.de>
2018-10-04 14:01:28 +0200

    index.js: Load all of Scrivito and Objs/Widgets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as Scrivito from "scrivito";
+import './Objs';
+import './Widgets';
 import App from './App';
 import "./config";
 import * as serviceWorker from './serviceWorker';
