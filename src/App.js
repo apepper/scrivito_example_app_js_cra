14e584194a652149b9e8787f6c89bf7f43d33e5e
Alexander Pepper <alexander.pepper@infopark.de>
2018-10-04 14:08:19 +0200

    App: Show full blown scrivito app.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as Scrivito from "scrivito";
+import CurrentPageMetaData from "./Components/CurrentPageMetaData";
+import ErrorBoundary from "./Components/ErrorBoundary";
+import Footer from "./Components/Footer";
+import GoogleAnalytics from "./Components/GoogleAnalytics";
+import Intercom from "./Components/Intercom";
+import Navigation from "./Components/Navigation";
+import NotFoundErrorPage from "./Components/NotFoundErrorPage";
+import CookieConsent from "./Components/CookieConsent";
 import './assets/stylesheets/index.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <ErrorBoundary>
+      <div>
+        <div className="content-wrapper">
+          <Navigation />
+          <Scrivito.CurrentPage />
+          <NotFoundErrorPage />
+        </div>
+        <Footer />
+        <CookieConsent />
+        <CurrentPageMetaData />
+        <GoogleAnalytics />
+        <Intercom />
       </div>
+    </ErrorBoundary>
     );
   }
 }
